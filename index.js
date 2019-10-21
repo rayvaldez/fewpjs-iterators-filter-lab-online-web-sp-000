d79cb6258f5b2cba5e7d0ce04fa369a046e461ea
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-10-21 17:21:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, name) {
+  return array.filter((driver) => {
+    return driver.toLowerCase() === name.toLowerCase()
+  })
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter((driver) => {
+    return driver.toLowerCase().slice(0, string.length) ===string.toLowerCase()
+  })
+}
+
+function matchName(array, name) {
+  return array.filter((driver) =>{
+    return driver.name.toLowerCase() === name.toLowerCase()
+  })
+}
